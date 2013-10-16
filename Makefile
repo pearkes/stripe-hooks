1bb8354b525dd7da90430bda3680b2c31c4f2d2d
Jack Pearkes <jackpearkes@gmail.com>
2013-10-16 16:06:47 +0200

    travis, and don't use forego for testing

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ run:
 	@DEBUG=True forego run python app.py
 
 test:
-	@ENVIRONMENT=test PYTHONPATH=. forego run py.test -v test/
+	@ENVIRONMENT=test PYTHONPATH=. py.test -v test/
 
 .PHONY: deps run test
 
--- settings.py
+++ settings.py
@@ -3,10 +3,9 @@ import os
 DEBUG = "DEBUG" in os.environ
 SECRET_KEY = os.environ.get("SECRET_KEY", "development")
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
-STRIPE_KEY = os.environ.get("STRIPE_KEY")
-STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
-AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
-AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+STRIPE_KEY = os.environ.get("STRIPE_KEY", "test")
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY", "test")
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY", "test")
 
 if ENVIRONMENT == 'test':
     TESTING = True
